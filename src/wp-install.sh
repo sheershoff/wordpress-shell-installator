2f2d960b7f6a272670a53102826198e02fbbc119
Ilya Sheershoff <sheershoff@gmail.com>
2015-11-06 12:27:13 +0500

    add composer installation

--- src/wp-install.sh
+++ src/wp-install.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
+curl -sS https://getcomposer.org/installer | php
 wget https://raw.githubusercontent.com/sheershoff/wordpress-shell-installator/master/src/wordpress/composer.json
-composer install
+php composer.phar install
 cd www
 wget https://raw.githubusercontent.com/sheershoff/wordpress-shell-installator/master/src/tnp/composer.json
-composer install
+php ../composer.phar install
 cd  ..
