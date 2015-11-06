bf305e785a49307df87d89db5b588143af8122c7
Ilya Sheershoff <sheershoff@gmail.com>
2015-11-06 12:19:32 +0500

    let install wp, themes and plugins through composer

---
+++ src/wp-install.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: sheershoff
+ * Date: 11/6/15
+ * Time: 12:53 PM
+ */
+
+`wget https://raw.githubusercontent.com/sheershoff/wordpress-shell-installator/master/src/wordpress/composer.json`;
+`composer install`;
+`cd www`;
+`wget https://raw.githubusercontent.com/sheershoff/wordpress-shell-installator/master/src/tnp/composer.json`;
+`composer install`;
